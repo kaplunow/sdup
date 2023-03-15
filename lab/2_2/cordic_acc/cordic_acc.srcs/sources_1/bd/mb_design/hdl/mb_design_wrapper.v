6a99b8305c37e90f763d99bdf9aa6f0fbcb44ba3
Wojciech Wolosz <wojciech.wolosz@racing.agh.edu.pl>
2023-03-15 19:52:44 +0100

    Done 4.3.10

---
+++ lab/2_2/cordic_acc/cordic_acc.srcs/sources_1/bd/mb_design/hdl/mb_design_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+//Date        : Wed Mar 15 19:42:09 2023
+//Host        : DESKTOP-J38DFM6 running 64-bit major release  (build 9200)
+//Command     : generate_target mb_design_wrapper.bd
+//Design      : mb_design_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module mb_design_wrapper
+   ();
+
+
+  mb_design mb_design_i
+       ();
+endmodule
