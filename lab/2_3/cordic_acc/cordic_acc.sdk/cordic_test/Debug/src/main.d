380f42526db9a11030db980fa61facbbb375c45e
Wojciech Wolosz <wojciech.wolosz@racing.agh.edu.pl>
2023-03-16 12:35:05 +0100

    done 6.1

---
+++ lab/2_3/cordic_acc/cordic_acc.sdk/cordic_test/Debug/src/main.d
@@ -0,0 +1,40 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../cordic_test_bsp/microblaze_0/include/xparameters.h \
+ ../../cordic_test_bsp/microblaze_0/include/xgpio.h \
+ ../../cordic_test_bsp/microblaze_0/include/xil_types.h \
+ ../../cordic_test_bsp/microblaze_0/include/xil_assert.h \
+ ../../cordic_test_bsp/microblaze_0/include/xstatus.h \
+ ../../cordic_test_bsp/microblaze_0/include/xgpio_l.h \
+ ../../cordic_test_bsp/microblaze_0/include/xil_io.h \
+ ../../cordic_test_bsp/microblaze_0/include/xil_printf.h \
+ ../../cordic_test_bsp/microblaze_0/include/xparameters.h \
+ ../../cordic_test_bsp/microblaze_0/include/bspconfig.h \
+ ../../cordic_test_bsp/microblaze_0/include/mb_interface.h \
+ ../../cordic_test_bsp/microblaze_0/include/xil_exception.h \
+ ../../cordic_test_bsp/microblaze_0/include/cordic_ip.h
+
+../../cordic_test_bsp/microblaze_0/include/xparameters.h:
+
+../../cordic_test_bsp/microblaze_0/include/xgpio.h:
+
+../../cordic_test_bsp/microblaze_0/include/xil_types.h:
+
+../../cordic_test_bsp/microblaze_0/include/xil_assert.h:
+
+../../cordic_test_bsp/microblaze_0/include/xstatus.h:
+
+../../cordic_test_bsp/microblaze_0/include/xgpio_l.h:
+
+../../cordic_test_bsp/microblaze_0/include/xil_io.h:
+
+../../cordic_test_bsp/microblaze_0/include/xil_printf.h:
+
+../../cordic_test_bsp/microblaze_0/include/xparameters.h:
+
+../../cordic_test_bsp/microblaze_0/include/bspconfig.h:
+
+../../cordic_test_bsp/microblaze_0/include/mb_interface.h:
+
+../../cordic_test_bsp/microblaze_0/include/xil_exception.h:
+
+../../cordic_test_bsp/microblaze_0/include/cordic_ip.h:
