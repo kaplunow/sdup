507cda6f674bd157b083de66f8ce401065db1656
Wojciech Wolosz <wojciech.wolosz@racing.agh.edu.pl>
2023-06-18 12:19:50 +0200

    Added rtl version with suprisingly well working simulation

--- lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/xsim_webtalk.tcl
+++ lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/sdup/lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sun Jun 18 11:10:49 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sun Jun 18 11:23:08 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "c871394a-b752-4c37-9c76-9fe715421317" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "f254d26754074cd8aaac55739f293f6c" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "15" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "17" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-4200H CPU @ 2.80GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.16_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6504_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 161872738 -regid "" -xml C:/sdup/lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/sdup/lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/sdup/lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6568_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3454024941 -regid "" -xml C:/sdup/lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/sdup/lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/sdup/lab/final_project/Taylor_mod/Taylor_mod.sim/sim_1/behav/xsim/xsim.dir/Taylor_mod_beh_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
---
+++ lab/final_project/Taylor_mod/Taylor_mod.srcs/sim_1/new/Taylor_mod_rtl_tb.sv
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+
+module Taylor_mod_rtl_tb;
+
+parameter integer WIDTH = 32;
+parameter MUL = 1073741824; //2^30
+parameter SHIFT = 30;
+
+reg clock, reset, start;
+reg [WIDTH-1:0] value_in;
+wire ready_out;
+wire[WIDTH-1:0] arccos_out;
+
+real arccos;
+
+Taylor_mod_rtl Taylor_mod_rtl(clock, reset, start, value_in, ready_out, arccos_out);
+
+//Clock generator
+initial
+    clock <= 1'b1;
+always
+ #1 clock <= ~clock;
+ 
+//Reset signal
+initial
+begin
+    reset <= 1'b1;
+#10 reset <= 1'b0;
+end 
+
+//Stimuli signals
+initial
+begin
+    value_in <= -0.5 * MUL; //Modify value in fixed-point [2:10] 
+    start <= 1'b0;
+#20 start <= 1'b1;
+#30 start <= 1'b0;
+end
+
+always @ (posedge ready_out)
+begin
+$display("RESULT: Calculated arccos for %f is equal to %f", value_in/MUL, arccos_out/MUL);
+end
+
+
+
+endmodule
